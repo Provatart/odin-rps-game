c34f08d30c5d208dcc4c3fabaef75a03a6a892cf
Artiom Provatorov <partobox@gmail.com>
2025-04-27 00:10:59 +0700

    all is done

--- js.js
+++ js.js
@@ -1,3 +1,40 @@
+let body = document.body;
+let overallResult = document.querySelector("#overallResult");
+let currentResult = document.querySelector("#currentResult");
+let score = document.querySelector("#score");
+let counter = 0;
+let total = {
+  user: 0,
+  comp: 0,
+};
+score.textContent = `Total score: user - ${total.user}; comp - ${total.comp}`;
+body.addEventListener("click", (e) => {
+  if (e.target.tagName === 'BUTTON'){
+  let usersChoice = e.target.textContent;
+  let compChoice = getComputerChoice();
+  // console.log(usersChoice, compChoice, score);
+    // 
+    currentResult.textContent = `Your choice is ${usersChoice} | Comp choice is ${compChoice} | Result of stage: ${checkResult(
+      usersChoice,
+      compChoice
+    )}`;
+    overallResult.textContent = ``
+    score.textContent = `Total score: user - ${total.user}; comp - ${total.comp}`;
+  if (total.user === 5 || total.comp === 5) {
+    let winner = (total.user ===5)?"YOU": "COMP";
+    total.user = 0;
+    total.comp = 0;
+    score.textContent = '';
+    // score.textContent = `Total score: user - ${total.user}; comp - ${total.comp}`;
+    currentResult.textContent += ` Game over.`;
+
+    
+    overallResult.textContent = `Winner: ${winner}`
+  }else{
+    }
+  }
+});
+
 function getComputerChoice() {
   let res = Math.floor(Math.random() * 3);
   if (res === 0) return "Rock";
@@ -7,12 +44,15 @@ function getComputerChoice() {
 function checkResult(userChoice, compChoice) {
   if (userChoice.toLowerCase() === "rock") {
     if (compChoice === "Rock") {
+
       return "Overall";
     }
     if (compChoice === "Scissors") {
+      total.user++;
       return "You win!";
     }
     if (compChoice === "Paper") {
+      total.comp++;
       return "Comp win!";
     }
   }
@@ -21,9 +61,11 @@ function checkResult(userChoice, compChoice) {
       return "Overall";
     }
     if (compChoice === "Rock") {
+      total.user++;
       return "You win!";
     }
     if (compChoice === "Scissors") {
+      total.comp++;
       return "Comp win!";
     }
   }
@@ -32,26 +74,30 @@ function checkResult(userChoice, compChoice) {
       return "Overall";
     }
     if (compChoice === "Paper") {
+      total.user++;
       return "You win!";
     }
     if (compChoice === "Rock") {
+      total.comp++;
       return "Comp win!";
     }
+   
   }
+  
 }
-let userChoice;
-let res;
-let compChoice;
-let cont;
-while (1) {
-  compChoice = getComputerChoice();
-  userChoice = prompt("Enter your choice");
-  console.log("Computer's choice:", compChoice);
-  console.log("User's choice:", userChoice);
-  console.log("Result:", checkResult(userChoice, compChoice));
-  cont = prompt("Continue?");
+// let userChoice;
+// let res;
+// let compChoice;
+// let cont;
+// while (1) {
+//   compChoice = getComputerChoice();
+//   userChoice = prompt("Enter your choice");
+//   console.log("Computer's choice:", compChoice);
+//   console.log("User's choice:", userChoice);
+//   console.log("Result:", checkResult(userChoice, compChoice));
+//   cont = prompt("Continue?");
 
-  if (cont === null || cont.toLowerCase() != "yes") {
-    break;
-  }
-}
+//   if (cont === null || cont.toLowerCase() != "yes") {
+//     break;
+//   }
+// }
